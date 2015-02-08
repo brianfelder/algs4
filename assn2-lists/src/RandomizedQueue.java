c17c07c32fe01c020c3a9e21b7c16b08319d3b2d
Felder, Brian <bfelder@cvent.com>
2015-02-07 17:09:13 -0800

    Empty RandomizedQueue

---
+++ assn2-lists/src/RandomizedQueue.java
@@ -0,0 +1,70 @@
+import java.util.Iterator;
+
+/**
+ * Created by bfelder on 2/7/15.
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    /**
+     * construct an empty randomized queue
+     */
+    public RandomizedQueue() {
+
+    }
+
+    /**
+     * is the queue empty?
+     * @return
+     */
+    public boolean isEmpty() {
+
+    }
+
+    /**
+     * return the number of items on the queue
+     * @return
+     */
+    public int size() {
+
+    }
+
+    /**
+     * add the item
+     * @param item
+     */
+    public void enqueue(Item item) {
+
+    }
+
+    /**
+     * delete and return a random item
+     * @return
+     */
+    public Item dequeue() {
+
+    }
+
+    /**
+     * return (but do not delete) a random item
+     * @return
+     */
+    public Item sample() {
+
+    }
+
+    /**
+     * return an independent iterator over items in random order
+     * @return
+     */
+    public Iterator<Item> iterator() {
+
+    }
+
+    /**
+     * unit testing
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
