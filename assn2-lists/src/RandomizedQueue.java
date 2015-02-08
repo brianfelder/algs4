8b983ff9a44981aa49e3f75d079d56ed764c78ff
Felder, Brian <bfelder@cvent.com>
2015-02-07 19:40:06 -0800

    Nonworking attempt with dequeue.

--- assn2-lists/src/RandomizedQueue.java
+++ assn2-lists/src/RandomizedQueue.java
@@ -5,11 +5,51 @@ import java.util.Iterator;
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
+    private Item[] items;
+    private int itemCount;
+    private int head;
+    private int tail;
+    private final static double sparsityThreshold = 0.25;
+
     /**
      * construct an empty randomized queue
      */
     public RandomizedQueue() {
+        items = (Item[])new Object[1];
+        head = 0;
+        tail = 0;
+        itemCount = 0;
 
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        int itemIndex = 0;
+        for (int i = head; i < tail; i++) {
+            if (items[i] != null) {
+                copy[itemIndex] = items[i];
+                itemIndex++;
+            }
+        }
+        // Reset head and tail.
+        tail = itemIndex;
+        head = 0;
+        itemCount = itemIndex;
+    }
+
+    /**
+     * returns the width of the distance between tail and head.
+     * @return
+     */
+    private int span() {
+        return tail - head;
+    }
+
+    private double sparsity() {
+        int span = span();
+        if (span == 0)
+            return 1.0;
+        return itemCount / span;
     }
 
     /**
@@ -17,7 +57,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      * @return
      */
     public boolean isEmpty() {
-
+        return itemCount == 0;
     }
 
     /**
@@ -25,7 +65,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      * @return
      */
     public int size() {
-
+        return itemCount;
     }
 
     /**
@@ -33,6 +73,13 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      * @param item
      */
     public void enqueue(Item item) {
+        if (tail == items.length)
+            resize(2 * items.length);
+        items[tail++] = item;
+        itemCount++;
+    }
+
+    private int nextRandomIndex() {
 
     }
 
@@ -41,7 +88,14 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      * @return
      */
     public Item dequeue() {
-
+        Item toReturn = items[--tail];
+        items[tail] = null;
+        if ((size() > 0) &&
+                (size() <= (items.length / 4)) ||
+                 sparsity() <= sparsityThreshold)
+            resize(items.length / 2);
+        itemCount--;
+        return toReturn;
     }
 
     /**
