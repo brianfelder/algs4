4442c56f4a8a88fc5384be0b94fae53dfebd3281
Felder, Brian <bfelder@cvent.com>
2015-02-17 19:38:33 -0800

    Fixed brute to order outputs.

--- assn3/src/Brute.java
+++ assn3/src/Brute.java
@@ -45,30 +45,32 @@ public class Brute {
         point1.drawTo(point2);
     }
 
-    private void printPointsInLine(Point point1, Point point2, Point point3, Point point4) {
+    private void printPointsInLine(Point [] fourPoints) {
         // TODO: Order sequence of points in line. Currently unordered.
-        System.out.printf("%s -> %s -> %s -> %s\n", point1.toString(), point2.toString(),
-                point3.toString(), point4.toString());
+        System.out.printf("%s -> %s -> %s -> %s\n", fourPoints[0].toString(), fourPoints[1].toString(),
+                fourPoints[2].toString(), fourPoints[3].toString());
     }
 
     public static void main(String[] args) {
         String fileName = args[0];
         Brute brute = new Brute();
         Point[] points = brute.readPointsFromFile(fileName);
+        Merge.sort(points);
         brute.drawAllPoints(points);
+        Point[] currentPoints = new Point[4];
         for (int i = 0; i < points.length - 3; i++) {
             for (int j = i + 1; j < points.length - 2; j++) {
                 for (int k = j + 1; k < points.length - 1; k++) {
                     for (int l = k + 1; l < points.length; l++) {
-                        Point point1 = points[i];
-                        Point point2 = points[j];
-                        Point point3 = points[k];
-                        Point point4 = points[l];
-                        double slope12 = point1.slopeTo(point2);
-                        double slope13 = point1.slopeTo(point3);
-                        double slope14 = point1.slopeTo(point4);
+                        currentPoints[0] = points[i];
+                        currentPoints[1] = points[j];
+                        currentPoints[2] = points[k];
+                        currentPoints[3] = points[l];
+                        double slope12 = currentPoints[0].slopeTo(currentPoints[1]);
+                        double slope13 = currentPoints[0].slopeTo(currentPoints[2]);
+                        double slope14 = currentPoints[0].slopeTo(currentPoints[3]);
                         if ((slope12 == slope13) && (slope13 == slope14)) {
-                            brute.printPointsInLine(point1, point2, point3, point4);
+                            brute.printPointsInLine(currentPoints);
                             //brute.drawLineFrom(point1, point2);
                             //brute.drawLineFrom(point1, point3);
                             //brute.drawLineFrom(point1, point4);
